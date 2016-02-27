c36a062fdf68caa99d2441efebcd7b249d95acf6
vale23 <valentina.rashevskaya@gmail.com>
2016-02-27 16:20:24 +0200

    add uploading file

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    file = models.ImageField(upload_to='images’, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
